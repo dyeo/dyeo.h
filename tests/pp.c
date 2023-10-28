ac4c09e7e03dbf7d8e65027754e994e11b47f917
danny@DANNYPC <danyeomans98@gmail.com>
2023-10-28 17:08:27 -0400

    Update pp.h

--- tests/pp.c
+++ tests/pp.c
@@ -1,10 +1,18 @@
-#include "pp.h"
+#include "../pp.h"
 
-#define Y(I, X) STR(I)":"STR(X),
-const char *s[] = {YEARGS(A,B,C,D,E,F)};
+#include "../c99.h"
+
+#define Y(I, X) STR(I) ":" STR(X),
+const char *s[] = {YEARGS(A, B, C, D, E, F)};
 #undef Y
+
+#define _fprintf_arg0 stdout
+#define _fprintf_arg1 "no args\n"
+#define fprintf(...) CALLFUNCD(fprintf, 2, __VA_ARGS__)
 
 int main()
 {
-    return 0;
+  fprintf();
+  fprintf(stderr, "args: %s\n", "b");
+  return 0;
 }
